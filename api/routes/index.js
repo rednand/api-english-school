2c13fc88131912cc6885296e36b809fa6d7978f4
Renan de Aguiar Modesto <renan.modesto@grupomultiplica.com.br>
2022-04-21 18:03:49 -0300

    adicionando novas tabelas e atualizando CRUD

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,8 +1,8 @@
 const bodyParser = require("body-parser");
-const pessoas = require("./pessoasRoutes");
+const pessoas = require("./pessoasRoutes.js");
+const niveis = require("./niveisRoutes.js");
+const turmas = require("./turmasRoute.js");
 
 module.exports = (app) => {
-  app.use(bodyParser.json());
-  app.use(pessoas);
+  app.use(bodyParser.json(), pessoas, niveis, turmas);
 };
-
--- api/routes/pessoasRoutes.js
+++ api/routes/pessoasRoutes.js
@@ -1,5 +1,5 @@
 const { Router } = require("express");
-const PessoaController = require("../controllers/PessoaController");
+const PessoaController = require("../controllers/PessoaController.js");
 
 const router = Router();
 
@@ -8,5 +8,18 @@ router.get("/pessoas/:id", PessoaController.pegaUmaPessoa);
 router.post("/pessoas", PessoaController.criaPessoa);
 router.put("/pessoas/:id", PessoaController.atualizaPessoa);
 router.delete("/pessoas/:id", PessoaController.apagaPessoa);
+router.get(
+  "/pessoas/:estudanteId/matricula/:matriculaId",
+  PessoaController.pegaUmaMatricula
+);
+router.post("/pessoas/:estudanteId/matricula", PessoaController.criaMatricula);
+router.put(
+  "/pessoas/:estudanteId/matricula/:matriculaId",
+  PessoaController.atualizaMatricula
+);
+router.delete(
+  "/pessoas/:estudanteId/matricula/:matriculaId",
+  PessoaController.apagaMatricula
+);
 
 module.exports = router;
